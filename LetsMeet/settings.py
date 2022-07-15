58d79e0609265fdd0ca1e44957d9763a78b0f1de
Aabhishek29 <starkabhishek29@gmail.com>
2022-07-15 23:44:47 +0530

    database migrated

--- LetsMeet/settings.py
+++ LetsMeet/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'LetsMeet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(BASE_DIR,'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,7 +118,23 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = 'starkabhishek29@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = "xquzloczyeavblaw"
--- Useraccount/models.py
+++ Useraccount/models.py
@@ -11,6 +11,7 @@ class Accounts(models.Model):
     email = models.EmailField(max_length=100, null=False, blank=False, unique=True)
 
     password = models.CharField(max_length=20, null=False, blank=False)
+    
     confirm_password = models.CharField(max_length=20, null=False, blank=False)
 
     # unique Id created by backend for identify person-to-person uniquely
